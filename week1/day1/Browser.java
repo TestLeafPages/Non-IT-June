f9c8ea14bc278ab4c8c6f154cce3087dc75a116a
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-07-09 19:37:49 +0530

    Add files via upload

---
+++ week1/day1/Browser.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Browser {
+	//method
+	public void driveCar() {
+		//variable
+		//print
+	}
+	
+//main method
+	public static void main(String[] args) {
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'm';
+		//print statement
+		
+		System.out.println("This is my first script");
+	}
+}
---
+++ week1/day1/Car.java
@@ -0,0 +1,22 @@
+package week1.day1;
+//Create class syntax
+public class Car {
+	//Global variable
+	//data type
+	byte wheel = 4;
+	short speed = 12399;
+	int insuranceNumber = 124044072;
+	long chasisNumber = 3545346363L;
+	float bankBalance = 10324.45f;
+	double kiloMeters = 324526.78d;
+	char model = 'M';
+	boolean airbag = true;
+	String name = "test leaf";
+//main method
+	public static void main(String[] args) {
+		int insuranceNumber = 124044072;
+		long chasisNumber = 3545346363L;
+		float bankBalance = 10324.45f;
+		System.out.println(insuranceNumber);
+	}
+}
